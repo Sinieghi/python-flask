4efc7391a0a5196de57c72b4cde8c09315daaed8
Luiz <l.guilherme128@gmail.com>
2024-01-03 16:25:34 -0300

    first commit

---
+++ server/src/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, make_response
+import os
+import time
+app = Flask(__name__)
+
+def format_time():
+    server_time = time.localtime()
+    return time.strftime("%I:%M:%S %p", server_time)
+@app.route("/")
+def index():
+    context = {'server_time': format_time()}
+    template = render_template("index.html", context=context)
+    response = make_response(template)
+    response.headers['Cache-control'] = 'public, max-age=300, s-maxage=600'
+    return response
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
